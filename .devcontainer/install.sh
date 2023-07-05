da11e38f025cc3a554d12a0172751c4b591a853f
Elliana May <me@mause.me>
2023-07-05 03:17:38 +0000

    preinstall docker?

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -4,4 +4,5 @@ apt-get update -y
 export DEBIAN_FRONTEND=noninteractive
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 apt-get -y install --no-install-recommends nodejs build-essential cmake ninja-build git valgrind ccache mold fish python3-dev python3-setuptools python3-pip cmake-curses-gui nodejs openjdk-17-jdk default-jdk gdb
+apt-get -y install docker docker-compose
 pip install clang-format==11.1.0.2 cmake-format
