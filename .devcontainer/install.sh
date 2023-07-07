f38b743b8a1564f44b3e27b3b7181c2d80cd738d
Elliana May <me@mause.me>
2023-07-07 06:36:35 +0000

    fix docker installation

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,8 +1,11 @@
 set -e
 
-apt-get update -y
 export DEBIAN_FRONTEND=noninteractive
+apt-get update -y
+apt-get upgrade -y
+apt-get install -y software-properties-common # for add-apt-repository
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" --yes
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-apt-get -y install --no-install-recommends nodejs build-essential cmake ninja-build git valgrind ccache mold fish python3-dev python3-setuptools python3-pip cmake-curses-gui nodejs openjdk-17-jdk default-jdk gdb
-apt-get -y install docker docker-compose
+apt-get -y install --no-install-recommends nodejs build-essential cmake ninja-build git valgrind ccache mold fish python3-dev python3-setuptools python3-pip cmake-curses-gui nodejs openjdk-17-jdk default-jdk gdb docker-ce
 pip install clang-format==11.1.0.2 cmake-format
